810cbda0cec1e511d86fb26f720147707c0210d0
Oscar De Alva <oscar.ivan98@gmail.com>
2020-05-30 16:54:11 -0500

    Changed styling for counter

--- counter.js
+++ counter.js
@@ -1,14 +1,14 @@
 const section = document.getElementById('counter');
-const text = document.createElement('h2');
+const text = document.createElement('h3');
 
 fetch('https://62n52ezwl8.execute-api.us-west-2.amazonaws.com/Prod/counter/')
     .then(res => res.json())
     .then(visitor => {
-        text.innerText = `You are visitor #${visitor.Visits}`;
+        text.innerText = `You are visitor #${visitor.Visits}.`;
         section.appendChild(text);
     })
     .catch(e => {
         console.error(e)
-        text.innerText = "There was an error obtaining visitors, sorry"
+        text.innerText = "There was an error obtaining visitors, sorry."
         section.appendChild(text);
     });
